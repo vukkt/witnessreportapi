6fdb5ede4839d6e782bf00bb1dc0a14d820c726e
Vuk Topalovic <63306506+vukkt@users.noreply.github.com>
2022-10-07 11:58:08 +0200

    final update

--- src/main/java/com/apiproject/witnessreportapi/model/LiveStream.java
+++ src/main/java/com/apiproject/witnessreportapi/model/LiveStream.java
@@ -4,4 +4,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record LiveStream(String id, String title, String description, String url, LocalDateTime) {
+
+    public LiveStream {
+        if (id==null){
+            throw new IllegalArgumentException("id cannot be null");
+            throw new IllegalArgumentException("id cannot be null");
+        }
+    }
 }
